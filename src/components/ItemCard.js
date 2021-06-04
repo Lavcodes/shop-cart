f3d25ba0d8c55fb7015d638048f1bad48f0994e7
Lavcodes <sanjali_b@ce.iitr.ac.in>
2021-06-05 03:56:36 +0530

    Listing Prod

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,1 +1,34 @@
 import React from 'react';
+
+const ItemCard = ({id, title, description, image, price}) => {
+    return (
+  <div class="ui card">
+  <div class="ui slide masked reveal image ">
+    <img src={image} class="visible content"/>
+    <div class="hidden content ui card">
+    <div class="content">
+      <div class="center aligned header bold">{title}</div>
+      
+      <div class="center aligned description">
+      {description}
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="content">
+    <a class="header">{title}</a>
+    <div class="meta">
+      $ {price}
+    </div>
+  </div>
+  <div class="ui violet animated button right floated circular">
+  <div class="hidden content">Shop</div>
+  <div class="visible content">
+    <i class="plus white icon"></i>
+  </div>
+</div>
+</div>
+  );
+};
+ 
+export default ItemCard;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import ItemCard from './ItemCard';
+
 const ProductList = (props) =>{
     console.log(props.products);
+    const List = props.products.map((product)=>{
+        return (
+            <ItemCard id = {product.id} 
+            title = {product.title} 
+            description = {product.description} 
+            image = {product.image}
+            price = {product.price}
+            >
+            </ItemCard>
+        );
+        
+    });
     return (
-        <div>Product List</div>
+        <div class="ui three stackable cards">{List}</div>
     );
 }
 
